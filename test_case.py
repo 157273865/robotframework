f92253627c2f1fae266d598c3ba9b3c14ba0832c
qlzhang <157273865@qq.com>
2016-11-04 22:58:30 +0800

    时间引用和格式展示

--- test_case.py
+++ test_case.py
@@ -1,2 +1,21 @@
-import os
-print "xitong!"
+import datetime
+import time
+import calendar
+
+# 获取此时的时间
+print time.localtime()
+
+# 获取当天的日期
+print datetime.datetime.now()
+print datetime.date.today()
+
+# 获取昨天的日期
+def getYesterday():
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=1)
+    yesterday = today - oneday
+    print type(today)     # 查看获取到时间的类型
+    print type(yesterday)
+    return yesterday
+yesterday = getYesterday()
+print "昨天的时间：", yesterday
